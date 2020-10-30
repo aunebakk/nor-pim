c698f4199138812d4ccb689f022f6dcf76c7183f
roger aunebakk <aunebakk@hotmail.com>
2020-10-30 08:48:34 +0100

    @dependabot rebase
    @dependabot rebase

--- Web/Scripts/Frameworks/bootstrap.js
+++ Web/Scripts/Frameworks/bootstrap.js
@@ -2,6 +2,7 @@
  * Bootstrap v3.3.7 (http://getbootstrap.com)
  * Copyright 2011-2016 Twitter, Inc.
  * Licensed under the MIT license
+ * @dependabot rebase
  */
 
 if (typeof jQuery === 'undefined') {
