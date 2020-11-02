3a5d0ccdc1ac59091ee0b25cd883f27c339e6eb0
aunebakk <aunebakk@hotmail.com>
2020-11-02 07:13:48 +0100

    sql2x task

--- Operational/Environment Switch.ps1
+++ Operational/Environment Switch.ps1
@@ -1,7 +1,7 @@
 ﻿# SQL2X Generated code based on a SQL Server Schema
 # SQL2X Version: 1.0
 # http://sql2x.org/
-# Generated Date: 11/2/2020 5:53:16 AM
+# Generated Date: 11/2/2020 6:12:29 AM
 # From Machine: DESKTOP-517I8BU
 # Template: SQL2XExtensionV3.SQL2XExtensionCreatorNorSolution.Content_EnvironmentSwitch
 param (
